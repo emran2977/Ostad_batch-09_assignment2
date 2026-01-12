51eecfb282b3870c17637dfe18f9f87a4f180f93
emran2977 <anikece846@gmail.com>
2026-01-12 07:42:04 +0600

    Update settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -25,17 +25,7 @@ SECRET_KEY = 'django-insecure-^=kn+1e3qi0rxqwk*bnt14qhnhl&xos(z&12@9+g3-2cg5o(4_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'subclimatic-sherri-nonprovocatively.ngrok-free.dev',
-    '.ngrok-free.dev',  # Allow all ngrok-free.dev subdomains
-    '.ngrok.io',  # Allow all ngrok.io subdomains (for paid ngrok)
-    'testtttttt-kf9v.onrender.com',
-    'ostad-batch-09-1-cwz9.onrender.com',
-    '.onrender.com',
-
-]
+ALLOWED_HOSTS = [*]
 
 # CSRF trusted origins for ngrok
 # Note: Django doesn't support wildcards in CSRF_TRUSTED_ORIGINS
